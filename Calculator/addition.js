74cd981e32643acf33819717bf4ad94e13c7d278
Akshata <118677433+A-K-x-hata@users.noreply.github.com>
2023-03-13 11:17:51 +0530

    Add files via upload

---
+++ Calculator/addition.js
@@ -0,0 +1,6 @@
+function myfunction() {
+    var var1 = parseInt(document.getElementById("first").value);
+    var var2 = parseInt(document.getElementById("second").value);
+    var var3 = var1 + var2;
+    document.getElementById("third").value = var3;
+}
